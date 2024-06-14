0290d3e49132de7a15400933caa68d418c452992
anas <anas.n.alhadi@outlook.com>
2024-06-14 20:24:01 -0300

    removed elf and other excutables :- -:

--- examples/intro/t4/bin/native/cpu/tramp.d
+++
@@ -1,5 +0,0 @@
-/home/cutie/RIOT/examples/tutorials/t4/bin/native/cpu/tramp.o: \
- /home/cutie/RIOT/cpu/native/tramp.S /usr/include/stdc-predef.h \
- /home/cutie/RIOT/examples/tutorials/t4/bin/native/riotbuild/riotbuild.h
-/usr/include/stdc-predef.h:
-/home/cutie/RIOT/examples/tutorials/t4/bin/native/riotbuild/riotbuild.h:
